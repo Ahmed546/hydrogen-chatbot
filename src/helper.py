354c5f93217050d99e8753eb5f3d2474102c9a48
Ahmed546 <ahmedwaleed0546@gmail.com>
2024-12-24 19:36:01 +0500

    error resolved

--- src/helper.py
+++ src/helper.py
@@ -1,5 +1,5 @@
 import PyPDF2
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
